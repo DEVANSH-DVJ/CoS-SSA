4cf020b9aa9fc34a45cb39a9ccfde0d4571c2cfe
devansh-dvj <devansh.dvj@gmail.com>
2022-05-18 15:40:43 +0530

    Updated error handler

--- error.cc
+++ error.cc
@@ -1,6 +1,21 @@
 #include "error.hh"
 
-string Backtrace(int skip = 1);
+extern FILE *cfg_out;
+
+extern string dot_file;
+extern fstream *dot_fd;
+
+void cleanup() {
+  cfg_out = freopen(NULL, "w", cfg_out);
+
+  dot_fd->close();
+  dot_fd->open(dot_file.c_str(), ios::out | ios::trunc);
+
+  fclose(cfg_out);
+  dot_fd->close();
+
+  exit(1);
+}
 
 void check_invariant_underlying_function(bool condition, string error_message,
                                          int line, string file) {
@@ -8,6 +23,13 @@ void check_invariant_underlying_function(bool condition, string error_message,
     cerr << "\nInternal error: " << error_message << " (Invariant at line "
          << line << ", file " << file << ").\n";
     print_stacktrace();
-    exit(1);
+    cleanup();
+  }
+}
+
+void report_violation_and_abort(bool condition, string error_message) {
+  if (!condition) {
+    cerr << "Semantic Error: " << error_message << "\n";
+    cleanup();
   }
 }
