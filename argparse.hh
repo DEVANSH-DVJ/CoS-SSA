0c2e1d61f22da4735a67151da197b5b27824e70a
devansh-dvj <devansh.dvj@gmail.com>
2022-05-18 23:01:59 +0530

    Added input type to argparse as compulsory option

--- argparse.hh
+++ argparse.hh
@@ -2,16 +2,21 @@
 #define _ARGPARSE_HH_
 
 #include <argp.h>
+#include <cstring>
 #include <string>
 
 using namespace std;
 
 enum short_options {
-  OPT_VISUALIZE = 0x80,
+  OPT_TYPE = 0x80,
+  OPT_VISUALIZE,
 };
+
+enum file_type { FILE_CFG, FILE_SSA, FILE_UNKNOWN };
 
 struct arguments {
   int visualize;
+  file_type input_type;
   string input_file;
 };
 
@@ -22,13 +27,28 @@ char doc[] = "CS-SSA";
 char args_doc[] = "[FILE]";
 
 /* Options visible to user */
-struct argp_option options[] = {{"visualize", OPT_VISUALIZE, 0, 0, "Visualize"},
+struct argp_option options[] = {{"type", OPT_TYPE, "TYPE", 0, "Type of input"},
+                                {"visualize", OPT_VISUALIZE, 0, 0, "Visualize"},
                                 {0}};
 
 error_t parse_opt(int key, char *arg, struct argp_state *state) {
   struct arguments *arguments = (struct arguments *)state->input;
 
   switch (key) {
+  case OPT_TYPE:
+    if (arg == NULL) {
+      cout << "Need to specify input type" << endl;
+      argp_usage(state);
+    } else if (strcmp(arg, "cfg") == 0) {
+      arguments->input_type = FILE_CFG;
+    } else if (strcmp(arg, "ssa") == 0) {
+      arguments->input_type = FILE_SSA;
+    } else {
+      cout << "Unknown input type: " << arg << endl;
+      argp_usage(state);
+    }
+    break;
+
   case OPT_VISUALIZE:
     arguments->visualize = 1;
     break;
@@ -41,6 +61,13 @@ error_t parse_opt(int key, char *arg, struct argp_state *state) {
 
   case ARGP_KEY_NO_ARGS:
     argp_usage(state); // no args
+    break;
+
+  case ARGP_KEY_END:
+    if (arguments->input_type == FILE_UNKNOWN) {
+      cout << "Need to specify input type" << endl;
+      argp_usage(state);
+    }
     break;
 
   default:
