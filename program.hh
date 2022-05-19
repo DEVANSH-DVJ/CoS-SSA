49832271e31146a22d2b4aeecf74f99fc2e28e0c
devansh-dvj <devansh.dvj@gmail.com>
2022-05-20 01:35:34 +0530

    Added base of program classes

--- program.hh
+++ program.hh
@@ -1,4 +1,22 @@
 #ifndef _PROGRAM_HH_
 #define _PROGRAM_HH_
 
+#include <iostream>
+#include <string>
+
+#include <map>
+
+using namespace std;
+
+#include "error.hh"
+#include "procedure.hh"
+
+class Program {
+public:
+  map<string, Procedure *> procedures;
+
+  Program();
+  ~Program();
+};
+
 #endif
