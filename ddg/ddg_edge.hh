54950ccd165f1bd7e39222d120518cca110262a8
devansh-dvj <devansh.dvj@gmail.com>
2022-05-19 09:48:00 +0530

    Added base of ddg classes

--- ddg/ddg_edge.hh
+++ ddg/ddg_edge.hh
@@ -1,4 +1,15 @@
 #ifndef _DDG_EDGE_HH_
 #define _DDG_EDGE_HH_
 
+#include "ddg_node.hh"
+
+class DDG_Edge {
+public:
+  DDG_Node *src;
+  DDG_Node *dst;
+
+  // DDG_Edge(DDG_Node *src, DDG_Node *dst);
+  // ~DDG_Edge();
+};
+
 #endif
--- ddg/ddg_node.hh
+++ ddg/ddg_node.hh
@@ -1,4 +1,21 @@
 #ifndef _DDG_NODE_HH_
 #define _DDG_NODE_HH_
 
+typedef enum {
+  DDG_Var,
+  DDG_Num,
+  DDG_Input,
+  DDG_Usevar,
+} DDG_NodeType;
+
+class DDG_Node {
+public:
+  DDG_NodeType type;
+  int node_id;
+  int meta_id;
+
+  // DDG_Node(DDG_NodeType type, int node_id, int meta_id);
+  // ~DDG_Node();
+};
+
 #endif
