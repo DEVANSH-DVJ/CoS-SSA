d9c4b8e1f76d8a9f81914b5127e9393cbe321ed5
devansh-dvj <devansh.dvj@gmail.com>
2022-05-20 01:35:28 +0530

    Added base of procedure classes

--- procedure.hh
+++ procedure.hh
@@ -1,4 +1,21 @@
 #ifndef _PROCEDURE_HH_
 #define _PROCEDURE_HH_
 
+#include <iostream>
+#include <string>
+
+#include <map>
+
+using namespace std;
+
+#include "error.hh"
+
+class Procedure {
+public:
+  string name;
+
+  Procedure(string name);
+  ~Procedure();
+};
+
 #endif
