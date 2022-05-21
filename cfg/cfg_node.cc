0ab3f762e64a930f79036f67d249d096a8bcc46e
devansh-dvj <devansh.dvj@gmail.com>
2022-05-21 13:41:43 +0530

    Added constructor to CFG Node class

--- cfg/cfg_node.cc
+++ cfg/cfg_node.cc
@@ -1,1 +1,15 @@
 #include "../headers.hh"
+
+using namespace std;
+
+CFG_Node::CFG_Node(CFG_NodeType type, int node_id) {
+  this->type = type;
+  this->node_id = node_id;
+  this->in_edges = new std::map<int, CFG_Edge *>();
+  this->out_edges = new std::map<int, CFG_Edge *>();
+}
+
+CFG_Node::~CFG_Node() {
+  delete this->in_edges;
+  delete this->out_edges;
+}
--- cfg/cfg_node.hh
+++ cfg/cfg_node.hh
@@ -1,6 +1,8 @@
 #ifndef _CFG_NODE_HH_
 #define _CFG_NODE_HH_
 
+#include <map>
+
 typedef enum {
   CFG_Start,
   CFG_End,
@@ -10,13 +12,18 @@ typedef enum {
   CFG_Expr,
 } CFG_NodeType;
 
+class CFG_Edge;
+
 class CFG_Node {
 public:
   CFG_NodeType type;
   int node_id;
 
-  // CFG_Node(CFG_NodeType type, int node_id);
-  // ~CFG_Node();
+  std::map<int, CFG_Edge *> *in_edges;
+  std::map<int, CFG_Edge *> *out_edges;
+
+  CFG_Node(CFG_NodeType type, int node_id);
+  ~CFG_Node();
 };
 
 #endif
