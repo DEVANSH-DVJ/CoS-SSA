ea47fc61ff8f53793c4f00f2a54d1619855a1232
devansh-dvj <devansh.dvj@gmail.com>
2022-05-21 17:59:32 +0530

    Added cfg class to procedure

--- procedure.cc
+++ procedure.cc
@@ -2,6 +2,28 @@
 
 using namespace std;
 
-Procedure::Procedure(string name) { this->name = name; }
+extern fstream *dot_fd;
+
+Procedure::Procedure(string name) {
+  this->name = name;
+  this->cfg_nodes = new map<int, CFG_Node *>();
+  this->cfg_edges = new map<pair<int, int>, CFG_Edge *>();
+}
 
 Procedure::~Procedure() {}
+
+void Procedure::visualize_cfg() {
+  *dot_fd << "digraph G {\n";
+  *dot_fd << "\tsubgraph cluster_" << this->name << " {\n";
+  *dot_fd << "\t\tlabel = \"" << this->name << "\";\n";
+  *dot_fd << "\t\tmargin = 20;\n";
+  for (map<int, CFG_Node *>::iterator it = this->cfg_nodes->begin();
+       it != this->cfg_nodes->end(); ++it) {
+    it->second->visualize();
+  }
+  for (map<pair<int, int>, CFG_Edge *>::iterator it = this->cfg_edges->begin();
+       it != this->cfg_edges->end(); ++it) {
+    it->second->visualize();
+  }
+  *dot_fd << "}\n";
+}
--- procedure.hh
+++ procedure.hh
@@ -1,14 +1,25 @@
 #ifndef _PROCEDURE_HH_
 #define _PROCEDURE_HH_
 
+#include <map>
+#include <utility>
+
 #include <string>
+
+#include "cfg/cfg_edge.hh"
+#include "cfg/cfg_node.hh"
 
 class Procedure {
 public:
   std::string name;
 
+  std::map<int, CFG_Node *> *cfg_nodes;
+  std::map<std::pair<int, int>, CFG_Edge *> *cfg_edges;
+
   Procedure(std::string name);
   ~Procedure();
+
+  void visualize_cfg();
 };
 
 #endif
