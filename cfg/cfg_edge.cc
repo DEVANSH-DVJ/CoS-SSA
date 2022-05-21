ac8b80a3e1de195620e6e9574df8f6f6bc56360f
devansh-dvj <devansh.dvj@gmail.com>
2022-05-21 13:41:32 +0530

    Added constructor to CFG Edge class

--- cfg/cfg_edge.cc
+++ cfg/cfg_edge.cc
@@ -1,1 +1,10 @@
 #include "../headers.hh"
+
+using namespace std;
+
+CFG_Edge::CFG_Edge(CFG_Node *src, CFG_Node *dst) {
+  this->src = src;
+  this->dst = dst;
+}
+
+CFG_Edge::~CFG_Edge() {}
--- cfg/cfg_edge.hh
+++ cfg/cfg_edge.hh
@@ -1,15 +1,15 @@
 #ifndef _CFG_EDGE_HH_
 #define _CFG_EDGE_HH_
 
-#include "cfg_node.hh"
+class CFG_Node;
 
 class CFG_Edge {
 public:
   CFG_Node *src;
   CFG_Node *dst;
 
-  // CFG_Edge(CFG_Node *src, CFG_Node *dst);
-  // ~CFG_Edge();
+  CFG_Edge(CFG_Node *src, CFG_Node *dst);
+  ~CFG_Edge();
 };
 
 #endif
