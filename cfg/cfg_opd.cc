6203d19f09ff8f28d3d66bf5a7ba7ff60a98048f
devansh-dvj <devansh.dvj@gmail.com>
2022-05-22 23:33:31 +0530

    Added constructors to CFG Opd

--- cfg/cfg_opd.cc
+++ cfg/cfg_opd.cc
@@ -1,1 +1,23 @@
 #include "../headers.hh"
+
+CFG_Opd::CFG_Opd(CFG_OpdType type, int num_value) {
+  this->type = type;
+
+  CHECK_INVARIANT(type == CFG_Num, "CFG_Num is expected");
+
+  this->num_value = num_value;
+  this->var_name = "";
+  this->str = to_string(num_value);
+}
+
+CFG_Opd::CFG_Opd(CFG_OpdType type, std::string var_name) {
+  this->type = type;
+
+  CHECK_INVARIANT(type == CFG_Var, "CFG_Var is expected");
+
+  this->num_value = 0;
+  this->var_name = var_name;
+  this->str = var_name;
+}
+
+CFG_Opd::~CFG_Opd() {}
