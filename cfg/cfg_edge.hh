f7a3140ddc516c04d0b69cd2777dce912c26f275
devansh-dvj <devansh.dvj@gmail.com>
2022-05-22 22:43:42 +0530

    Added id to CFG Edge

--- cfg/cfg_edge.hh
+++ cfg/cfg_edge.hh
@@ -1,10 +1,13 @@
 #ifndef _CFG_EDGE_HH_
 #define _CFG_EDGE_HH_
 
+#include <utility>
+
 class CFG_Node;
 
 class CFG_Edge {
 public:
+  std::pair<int, int> edge_id;
   CFG_Node *src;
   CFG_Node *dst;
 
