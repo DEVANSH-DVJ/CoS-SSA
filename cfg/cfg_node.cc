ffd1bb941a98805b38f18318931ef40472f2f243
devansh-dvj <devansh.dvj@gmail.com>
2022-05-22 23:23:29 +0530

    Updated constructor of CFG Node

--- cfg/cfg_node.cc
+++ cfg/cfg_node.cc
@@ -7,8 +7,17 @@ extern fstream *dot_fd;
 CFG_Node::CFG_Node(CFG_NodeType type, int node_id) {
   this->type = type;
   this->node_id = node_id;
+
+  this->parent_proc = "";
+  this->stmt = "";
   this->in_edges = new std::map<int, CFG_Edge *>();
   this->out_edges = new std::map<int, CFG_Edge *>();
+
+  this->callee_proc = "";
+  this->lopd = "";
+  this->ropd1 = "";
+  this->ropd2 = "";
+  this->op = "";
 }
 
 CFG_Node::~CFG_Node() {
