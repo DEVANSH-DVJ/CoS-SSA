6184c6a36971a36d672cc3add9dc6c4a701bef5f
devansh-dvj <devansh.dvj@gmail.com>
2022-05-23 13:51:33 +0530

    Added SSA Node and Edge to Program

--- program.cc
+++ program.cc
@@ -9,6 +9,8 @@ Program::Program() {
   this->procedures = new map<string, Procedure *>();
   this->cfg_nodes = new map<int, CFG_Node *>();
   this->cfg_edges = new map<pair<int, int>, CFG_Edge *>();
+  this->ssa_nodes = new map<int, SSA_Node *>();
+  this->ssa_edges = new map<pair<int, int>, SSA_Edge *>();
 }
 
 Program::~Program() {
@@ -29,6 +31,18 @@ Program::~Program() {
     delete it->second;
   }
   delete this->cfg_edges;
+
+  for (map<int, SSA_Node *>::iterator it = this->ssa_nodes->begin();
+       it != this->ssa_nodes->end(); ++it) {
+    delete it->second;
+  }
+  delete this->ssa_nodes;
+
+  for (map<pair<int, int>, SSA_Edge *>::iterator it = this->ssa_edges->begin();
+       it != this->ssa_edges->end(); ++it) {
+    delete it->second;
+  }
+  delete this->ssa_edges;
 }
 
 Procedure *Program::get_proc(string name) {
@@ -44,6 +58,14 @@ CFG_Node *Program::get_cfg_node(int node_id) {
                           "CFG node " + to_string(node_id) + " not found.");
   }
   return this->cfg_nodes->find(node_id)->second;
+}
+
+SSA_Node *Program::get_ssa_node(int node_id) {
+  if (this->ssa_nodes->find(node_id) == this->ssa_nodes->end()) {
+    CHECK_INPUT_AND_ABORT(false,
+                          "SSA node " + to_string(node_id) + " not found.");
+  }
+  return this->ssa_nodes->find(node_id)->second;
 }
 
 void Program::visualize_cfg() {
--- program.hh
+++ program.hh
@@ -8,6 +8,8 @@
 class Procedure;
 class CFG_Edge;
 class CFG_Node;
+class SSA_Edge;
+class SSA_Node;
 
 class Program {
 public:
@@ -17,11 +19,15 @@ public:
   std::map<int, CFG_Node *> *cfg_nodes;
   std::map<std::pair<int, int>, CFG_Edge *> *cfg_edges;
 
+  std::map<int, SSA_Node *> *ssa_nodes;
+  std::map<std::pair<int, int>, SSA_Edge *> *ssa_edges;
+
   Program();
   ~Program();
 
   Procedure *get_proc(string name);
   CFG_Node *get_cfg_node(int node_id);
+  SSA_Node *get_ssa_node(int node_id);
 
   void visualize_cfg();
 };
