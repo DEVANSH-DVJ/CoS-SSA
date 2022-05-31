a9143b43d08971015d833262489ffde1684c342c
devansh-dvj <devansh.dvj@gmail.com>
2022-05-31 23:11:24 +0530

    Updated run script

--- run.sh
+++ run.sh
@@ -1,10 +1,18 @@
 #/bin/sh
 
 make clean && make
-./cs_ssa --type=cfg tests/0.cfg
-./cs_ssa --type=cfg tests/1.cfg
-./cs_ssa --type=cfg tests/2.cfg
-./cs_ssa --type=cfg tests/3.cfg
-./cs_ssa --type=cfg tests/4.cfg
-./cs_ssa --type=cfg tests/5.cfg
-./cs_ssa --type=ssa tests/0.ssa
+
+if [[ -f cs_ssa ]]; then
+  # CFG Tests
+  ./cs_ssa --type=cfg tests/0.cfg
+  ./cs_ssa --type=cfg tests/1.cfg
+  ./cs_ssa --type=cfg tests/2.cfg
+  ./cs_ssa --type=cfg tests/3.cfg
+  ./cs_ssa --type=cfg tests/4.cfg
+  ./cs_ssa --type=cfg tests/5.cfg
+
+  # SSA Tests
+  ./cs_ssa --type=ssa tests/0.ssa
+else
+  echo "Compilation Failed!"
+fi
