eacd73586fc690221faa5e6157c065c0fd6b1874
devansh-dvj <devansh.dvj@gmail.com>
2022-05-31 22:53:34 +0530

    Added get and update functions to Procedure

--- procedure.hh
+++ procedure.hh
@@ -15,19 +15,40 @@ class Procedure;
 
 class Procedure {
 public:
+  /* Always initialized: by constructor */
   std::string name;
 
+  /* CFG Graph */
   std::map<int, CFG_Node *> *cfg_nodes;
   std::map<std::pair<int, int>, CFG_Edge *> *cfg_edges;
 
+  /* SSA Graph */
   std::map<int, SSA_Node *> *ssa_nodes;
   std::map<std::pair<int, int>, SSA_Edge *> *ssa_edges;
 
+  /* Constructors and Destructor */
   Procedure(std::string name);
   ~Procedure();
 
-  void visualize_cfg();
-  void visualize_ssa();
+  /* Get functions */
+  // Get procedure name
+  std::string get_name() const;
+
+  /* Update functions */
+  // Add a CFG node
+  void add_cfg_node(CFG_Node *node) const;
+  // Add a CFG edge
+  void add_cfg_edge(CFG_Edge *edge) const;
+  // Add a SSA node
+  void add_ssa_node(SSA_Node *node) const;
+  // Add a SSA edge
+  void add_ssa_edge(SSA_Edge *edge) const;
+
+  /* Helper functions */
+  // Visualize CFG graph
+  void visualize_cfg() const;
+  // Visualize SSA graph
+  void visualize_ssa() const;
 };
 
 #endif
