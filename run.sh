2503a0882f29f9e2513a0741b639e3bbfa8c00df
devansh-dvj <devansh.dvj@gmail.com>
2022-06-01 09:29:36 +0530

    Updated run script

--- run.sh
+++ run.sh
@@ -10,9 +10,11 @@ if [[ -f cs_ssa ]]; then
   ./cs_ssa --type=cfg tests/3.cfg
   ./cs_ssa --type=cfg tests/4.cfg
   ./cs_ssa --type=cfg tests/5.cfg
+  ./cs_ssa --type=cfg tests/6.cfg
 
   # SSA Tests
   ./cs_ssa --type=ssa tests/0.ssa
+  ./cs_ssa --type=ssa tests/6.ssa
 else
   echo "Compilation Failed!"
 fi
