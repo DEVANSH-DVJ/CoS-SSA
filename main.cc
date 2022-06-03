320589eadecba185e583675dc65185b9a641de9a
devansh-dvj <devansh.dvj@gmail.com>
2022-06-04 00:11:34 +0530

    Added tool types to main

--- main.cc
+++ main.cc
@@ -21,10 +21,20 @@ int main(int argc, char **argv) {
   // Default arguments
   struct arguments arguments;
 
-  arguments.input_type = FILE_UNKNOWN;
+  arguments.tool = TOOL_UNKNOWN;
   arguments.input_file = "";
 
   argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+  string input_file = arguments.input_file;
+  switch (arguments.tool) {
+  case TOOL_CFG:
+    break;
+  case TOOL_SSA:
+    break;
+  default:
+    CHECK_INVARIANT(CONTROL_SHOULD_NOT_REACH, "Unknown tool");
+  }
 
   exit(0);
 }
