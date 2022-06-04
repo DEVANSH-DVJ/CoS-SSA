f197c0b1a4117086b7d3ba5c999e57f128abe07d
devansh-dvj <devansh.dvj@gmail.com>
2022-06-05 00:42:03 +0530

    Updated run script

--- run.sh
+++ run.sh
@@ -4,17 +4,17 @@ make
 
 if [[ -f cs_ssa ]]; then
   # CFG Tests
-  ./cs_ssa --type=cfg tests/0.cfg
-  ./cs_ssa --type=cfg tests/1.cfg
-  ./cs_ssa --type=cfg tests/2.cfg
-  ./cs_ssa --type=cfg tests/3.cfg
-  ./cs_ssa --type=cfg tests/4.cfg
-  ./cs_ssa --type=cfg tests/5.cfg
-  ./cs_ssa --type=cfg tests/6.cfg
+  ./cs_ssa --tool=cfg tests/0.cfg
+  ./cs_ssa --tool=cfg tests/1.cfg
+  ./cs_ssa --tool=cfg tests/2.cfg
+  ./cs_ssa --tool=cfg tests/3.cfg
+  ./cs_ssa --tool=cfg tests/4.cfg
+  ./cs_ssa --tool=cfg tests/5.cfg
+  ./cs_ssa --tool=cfg tests/6.cfg
 
   # SSA Tests
-  ./cs_ssa --type=ssa tests/0.ssa
-  ./cs_ssa --type=ssa tests/6.ssa
+  ./cs_ssa --tool=ssa tests/0.ssa
+  ./cs_ssa --tool=ssa tests/6.ssa
 else
   echo "Compilation Failed!"
 fi
