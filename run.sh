66364b09b8ea3f27e859be2574c96d3b43980292
devansh-dvj <devansh.dvj@gmail.com>
2022-09-30 20:56:53 +0530

    Updated run script

--- run.sh
+++ run.sh
@@ -3,18 +3,25 @@
 make DEBUG=0
 
 if [[ -f cs_ssa ]]; then
-  # CFG Tests
-  ./cs_ssa --tool=cfg tests/0.cfg
-  ./cs_ssa --tool=cfg tests/1.cfg
-  ./cs_ssa --tool=cfg tests/2.cfg
-  ./cs_ssa --tool=cfg tests/3.cfg
-  ./cs_ssa --tool=cfg tests/4.cfg
-  ./cs_ssa --tool=cfg tests/5.cfg
-  ./cs_ssa --tool=cfg tests/6.cfg
+    # CFG Tests
+    ./cs_ssa --tool=cfg tests/0.cfg
+    ./cs_ssa --tool=cfg tests/1.cfg
+    ./cs_ssa --tool=cfg tests/2.cfg
+    ./cs_ssa --tool=cfg tests/3.cfg
+    ./cs_ssa --tool=cfg tests/4.cfg
+    ./cs_ssa --tool=cfg tests/5.cfg
+    ./cs_ssa --tool=cfg tests/6.cfg
+    ./cs_ssa --tool=cfg tests/7_orig.cfg
+    ./cs_ssa --tool=cfg tests/7_dupl.cfg
+    ./cs_ssa --tool=cfg tests/8.cfg
+    ./cs_ssa --tool=cfg tests/9.cfg
+    ./cs_ssa --tool=cfg tests/10.cfg
 
-  # SSA Tests
-  ./cs_ssa --tool=ssa tests/0.ssa
-  ./cs_ssa --tool=ssa tests/6.ssa
+    # SSA Tests
+    ./cs_ssa --tool=ssa tests/0.ssa
+    ./cs_ssa --tool=ssa tests/6.ssa
+    ./cs_ssa --tool=ssa tests/7_orig.ssa
+    ./cs_ssa --tool=ssa tests/7_exp.ssa
 else
-  echo "Compilation Failed!"
+    echo "Compilation Failed!"
 fi
